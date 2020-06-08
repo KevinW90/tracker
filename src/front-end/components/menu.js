7f4c12b8d0e7dd7cbad30f8b7db4c6af687dbc75
KevinW90 <kevinjwilliams76@gmail.com>
2020-06-08 11:29:44 -0400

    changed menu from left to top

--- src/front-end/components/menu.js
+++ src/front-end/components/menu.js
@@ -6,14 +6,16 @@ const menu = (props) => {
   return (
     <div className="menu">
       <div className="logo">
+        <span className="title">bug</span>
         <i className="fas fa-bug"></i>
+        <span className="title">squisher</span>
       </div>
 
       <div className="menu-items">
         <div className="menu-item">
           <div className="btn" onClick={props.createBug}>
             <i className="fas fa-plus"></i>
-            Add Bug
+            Create Bug
           </div>
         </div>
       </div>
